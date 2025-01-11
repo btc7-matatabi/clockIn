c0f0cfe6064b13d08521ec38b6b362c6dca5e090
t-kunita <tomoko_kunita@mail.toyota.co.jp>
2025-01-11 23:04:49 +0900

    QR ver

--- frontend/components/02_QrScanScreen.tsx
+++ frontend/components/02_QrScanScreen.tsx
@@ -4,15 +4,15 @@ import { useAtom } from "jotai";
 import { employeeCodeAtom, UserInfos, userInfosAtom } from "../src/atoms";
 import { Box, Typography } from "@mui/material";
 import { AppToolBar } from "../src/AppToolBar.tsx";
-// import {useEffect} from "react";
-import {useEffect, useRef} from "react";
+import {useEffect} from "react";
+// import {useEffect, useRef} from "react";
 
 export function QrScanScreen() {
   const navigate = useNavigate();
   const [, setEmployeeCode] = useAtom(employeeCodeAtom);
   const [, setUserInfos] = useAtom(userInfosAtom);
 
-    const inputRef = useRef<HTMLInputElement>(null);  //暫定
+    // const inputRef = useRef<HTMLInputElement>(null);  //暫定
 
   async function getUserInfo(empCd: string) {
     const URL = process.env.VITE_URL;
@@ -73,14 +73,14 @@ export function QrScanScreen() {
         };
     }, []);
   // 暫定「仮）QR読込」ボタン用処理
-  const handleButtonClick = () => {
-      const userId = inputRef.current?.value;
-      console.log("userId",userId)
-    if(userId){
-      getUserInfo(userId);
-      navigate("/time-select");
-    }
-  };
+  // const handleButtonClick = () => {
+  //     const userId = inputRef.current?.value;
+  //     console.log("userId",userId)
+  //   if(userId){
+  //     getUserInfo(userId);
+  //     navigate("/time-select");
+  //   }
+  // };
 
   return (
     <>
@@ -100,86 +100,86 @@ export function QrScanScreen() {
           }}
         >
             {/*暫定*/}
-          {/*<Typography*/}
-          {/*  variant="h5"*/}
-          {/*  sx={{*/}
-          {/*    marginTop: "10px",*/}
-          {/*    fontWeight: 600,*/}
-          {/*    flexGrow: 1,*/}
-          {/*    textAlign: "center",*/}
-          {/*  }}*/}
-          {/*>*/}
-          {/*  QRコードを読み込ませてください*/}
-          {/*</Typography>*/}
+          <Typography
+            variant="h5"
+            sx={{
+              marginTop: "10px",
+              fontWeight: 600,
+              flexGrow: 1,
+              textAlign: "center",
+                height:"40px"
+            }}
+          >
+            QRコードを読み込ませてください
+          </Typography>
 
+          <video
+            ref={ref}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              margin: 0,
+            }}
+          />
 
-          {/*<video*/}
-          {/*  ref={ref}*/}
-          {/*  style={{*/}
-          {/*    width: "100%",*/}
-          {/*    height: "100%",*/}
-          {/*    objectFit: "cover",*/}
-          {/*    margin: 0,*/}
-          {/*  }}*/}
-          {/*/>*/}
+            {/*<Box*/}
+            {/*    sx={{*/}
+            {/*        display: "flex",*/}
+            {/*        flexDirection: "column",*/}
+            {/*        alignItems: "center",*/}
+            {/*        // justifyContent: "center",*/}
+            {/*        gap: "20px", // 要素間にスペースを入れる*/}
+            {/*        textAlign: "center",*/}
+            {/*        flexGrow: 1, // 親のflexbox内で中央に配置*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    <Typography*/}
+            {/*        variant="h2"*/}
+            {/*        sx={{*/}
+            {/*            marginTop: "100px",*/}
+            {/*            fontWeight: 600,*/}
+            {/*            // flexGrow: 1,*/}
+            {/*            textAlign: "center",*/}
 
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    // justifyContent: "center",
-                    gap: "20px", // 要素間にスペースを入れる
-                    textAlign: "center",
-                    flexGrow: 1, // 親のflexbox内で中央に配置
-                }}
-            >
-                <Typography
-                    variant="h2"
-                    sx={{
-                        marginTop: "100px",
-                        fontWeight: 600,
-                        // flexGrow: 1,
-                        textAlign: "center",
-
-                    }}
-                >
-                    従業員コード
-                </Typography>
-                    <input
-                        type="text"
-                        id="userId"
-                        name="userId"
-                        // placeholder="ユーザーID"
-                        ref={inputRef}
-                        style={{
-                            padding: "10px",
-                            fontSize: "32px",
-                            borderRadius: "5px",
-                            width: "500px",
-                            height:"100px",
-                            margin:"50px",
-                        }}
-                    />
-                    <button type="submit"
-                            className="login-button"
-                            onClick={handleButtonClick}
-                            style={{
-                                margin:"50px",
-                            padding: "10px 20px",
-                            fontSize: "48px",
-                            backgroundColor: "#909294",
-                            color: "white",
-                            borderRadius: "5px",
-                            border: "none",
-                            cursor: "pointer",
-                            // width: "80%", // ボタン幅を調整
-                            width: "500px", // 最大幅を指定
-                                height:"100px"
-                        }}>
-                        ログイン
-                    </button>
-            </Box>
+            {/*        }}*/}
+            {/*    >*/}
+            {/*        従業員コード*/}
+            {/*    </Typography>*/}
+            {/*        <input*/}
+            {/*            type="text"*/}
+            {/*            id="userId"*/}
+            {/*            name="userId"*/}
+            {/*            // placeholder="ユーザーID"*/}
+            {/*            ref={inputRef}*/}
+            {/*            style={{*/}
+            {/*                padding: "10px",*/}
+            {/*                fontSize: "32px",*/}
+            {/*                borderRadius: "5px",*/}
+            {/*                width: "500px",*/}
+            {/*                height:"100px",*/}
+            {/*                margin:"50px",*/}
+            {/*            }}*/}
+            {/*        />*/}
+            {/*        <button type="submit"*/}
+            {/*                className="login-button"*/}
+            {/*                onClick={handleButtonClick}*/}
+            {/*                style={{*/}
+            {/*                    margin:"50px",*/}
+            {/*                padding: "10px 20px",*/}
+            {/*                fontSize: "48px",*/}
+            {/*                backgroundColor: "#909294",*/}
+            {/*                color: "white",*/}
+            {/*                borderRadius: "5px",*/}
+            {/*                border: "none",*/}
+            {/*                cursor: "pointer",*/}
+            {/*                // width: "80%", // ボタン幅を調整*/}
+            {/*                width: "500px", // 最大幅を指定*/}
+            {/*                    height:"100px"*/}
+            {/*            }}>*/}
+            {/*            ログイン*/}
+            {/*        </button>*/}
+            {/*</Box>*/}
 
 
         </Box>
